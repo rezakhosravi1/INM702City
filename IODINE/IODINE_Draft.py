0db7a757bc126d10c95651145e27a51738700854
Reza Khosravi <khosravi.mrk@gmail.com>
2021-01-11 14:17:31 +0300

    corrections for mixture and elbo

--- IODINE/IODINE_Draft.py
+++ IODINE/IODINE_Draft.py
@@ -21,7 +21,6 @@ class Sampling(tfkl.Layer):
 @tf.function
 def my_exponential_linear_unit(input):
     return (tfk.backend.elu(input) + tfk.backend.epsilon())
-    # return (tfk.backend.elu(input) + 1.0 + tfk.backend.epsilon())
 
 batch_size = 32
 T = 5
@@ -61,38 +60,6 @@ class SpatialBroadCast(tfkl.Layer):
         return z_spatial_broadcast
 
 
-@tf.function
-def mixture_components_generator(logits, components):
-    tetramix = tfd.Mixture(
-        cat=tfd.Categorical(logits=logits),
-        components=[tfd.Normal(loc=components[0,:,:,:], scale=0.1),
-                    tfd.Normal(loc=components[1,:,:,:], scale=0.1),
-                    tfd.Normal(loc=components[2,:,:,:], scale=0.1),
-                    tfd.Normal(loc=components[3,:,:,:], scale=0.1)]    
-        )
-    return tetramix.sample()
-
-class MixtureDensity(tfkl.Layer):
-
-    # DIFNE LIKELIHOOD BECUASE OF TF.FUNCTION-DECORATOR 
-    # YOU CANNOT DEFINE A VARIABLE MORE THAN ONCE IN GRAPH MODE
-    def __init__(self, name):
-        super().__init__(name,)
-        self.likelihood = None
-
-    @tf.function
-    def call(self, inputs):
-        mus, masks_logits = tf.split(inputs, num_or_size_splits=[3,1], axis=-1)
-        if self.likelihood is None:
-            self.likelihood = tf.Variable(tf.zeros(shape=(mus.shape[0],mus.shape[2],mus.shape[3],mus.shape[4]),
-                                              dtype=np.float32))
-        masks = tf.nn.softmax(masks_logits, axis=1)
-        masks_tiled_transposed = tf.transpose(tfk.backend.tile(masks, (1,1,1,1,3)), [0,2,3,4,1])
-        for i in tf.range(tf.shape(mus)[0]):
-            self.likelihood[i,:,:,:].assign(mixture_components_generator(logits=masks_tiled_transposed[i,:,:,:,:],
-                                                                            components=mus[i,:,:,:,:]))
-        return masks_logits, masks, mus, self.likelihood
-
 # DECODER 
 latent_inputs = tfk.Input(shape=(K,latent_dim,),
                           name='z_samling')
@@ -107,13 +74,9 @@ decoder_output = tfkl.Conv2D(filters=32, kernel_size=kernel_size, padding='same'
                 activation=my_exponential_linear_unit, name='decoder_conv_4')(decoder_output)
 rgb_mask = tfkl.Conv2D(filters=4, kernel_size=kernel_size, padding='same', 
                 activation=tfk.activations.linear, name='decoder_conv_5')(decoder_output)
-masks_logits, masks, mus, likelihood = MixtureDensity(name='Mixture_Density')(rgb_mask)
 generator = tfk.Model(inputs=latent_inputs,
-                    outputs=[masks_logits,
-                             masks,
-                             mus,
-                             likelihood],
-                    name='generator')
+                      outputs=rgb_mask,
+                      name='generator')
 
 # %%
 # # DEFINE THE SLOTS AND RECONSTRUCTION MODEL
@@ -142,8 +105,12 @@ likelihood = tfk.Input(shape=(IMAGE_SIZE[0],IMAGE_SIZE[1],3),
                    name='likelihood')
 mus_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],3),
                    name='mus')
+mus_gradient_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],3),
+                   name='mus_gradient')
 masks_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],1),
                    name='masks')
+masks_gradient_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],1),
+                   name='masks_gradient')
 masks_logits_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],1),
                    name='masks_logits')
 x_coordinate_input = tfk.Input(shape=(K,IMAGE_SIZE[0],IMAGE_SIZE[1],1),
@@ -162,7 +129,9 @@ likelihood_expanded = Expand(name='likelihood_expanded')(likelihood)
 encoder_inputs = tfkl.concatenate([original_image_expanded,
                                    likelihood_expanded,
                                    mus_input,
+                                   mus_gradient_input,
                                    masks_input,
+                                   masks_gradient_input,
                                    masks_logits_input,
                                    x_coordinate_input,
                                    y_coordinate_input])
@@ -212,7 +181,9 @@ z_mean, z_log_var, z = Sampling()(encoder_output)
 refinement = tfk.Model(inputs=[original_image,
                                likelihood,
                                mus_input,
+                               mus_gradient_input,
                                masks_input,
+                               masks_gradient_input,
                                masks_logits_input,
                                x_coordinate_input,
                                y_coordinate_input,
@@ -226,7 +197,7 @@ refinement = tfk.Model(inputs=[original_image,
 @tf.function
 def log_normal_pdf(z, z_mean, z_log_var):
     return tf.reduce_sum(
-        -0.5 * ((z - z_mean)) ** 2. * tf.exp(-z_log_var) + z_log_var + tf.math.log(2.* math.pi),
+        -0.5 * ((z - z_mean) ** 2. * tf.exp(-z_log_var) + z_log_var + tf.math.log(2.* math.pi)),
         axis=[1,2])
 
 # DEFINE THE METRIC(S) WE WISH TO TRACK AND WRITE THE SUMMARY
@@ -245,10 +216,13 @@ class VAE(tfk.Model):
         # NORMALIZE THROUGH ALL CHANNELS AND SPATIAL DIMS AND POSTERIOR GRADIENT
         self.layer_normalization_likelihood = tfkl.LayerNormalization(axis=[1,2,3])
         self.layer_normalization_posterior_gradient = tfkl.LayerNormalization(axis=-1)
+        self.layer_normalization_mus_gradient = tfkl.LayerNormalization(axis=[2,3,4])
+        self.layer_normalization_masks_gradient = tfkl.LayerNormalization(axis=-1)
 
-    def compile(self, optimizer, loss_fn):
+    def compile(self, g_optimizer, ref_optimizer, loss_fn):
         super().compile()
-        self.optimizer = optimizer
+        self.g_optimizer = g_optimizer
+        self.ref_optimizer = ref_optimizer
         self.loss_fn = loss_fn
     
     
@@ -264,7 +238,12 @@ class VAE(tfk.Model):
                                                       posterior_log_var],
                                                       axis=-1))
         
-        masks_logits, masks, mus, likelihood = self.generator(posterior)
+        rgb_mask = self.generator(posterior)
+        mus, masks_logits = tf.split(rgb_mask, num_or_size_splits=[3,1], axis=-1)
+        masks = tf.nn.softmax(masks_logits, axis=1)
+        masks_tiled = tfk.backend.tile(masks, (1,1,1,1,3))
+        epsilon = tfk.backend.random_normal(shape=tf.shape(mus))
+        likelihood = tf.reduce_sum(masks_tiled * (mus + tf.exp(0.5 * 0) * epsilon), axis=1)
 
         with tf.GradientTape(persistent=True) as tape:
             
@@ -275,28 +254,36 @@ class VAE(tfk.Model):
                         posterior,
                         self.generator.trainable_variables])
             
-            masks_logits, masks, mus, likelihood = self.generator(posterior)
+            rgb_mask = self.generator(posterior)
+            mus, masks_logits = tf.split(rgb_mask, num_or_size_splits=[3,1], axis=-1)
+            masks = tf.nn.softmax(masks_logits, axis=1)
+            masks_tiled = tfk.backend.tile(masks, (1,1,1,1,3))
+            epsilon = tfk.backend.random_normal(shape=tf.shape(mus))
+            likelihood = tf.reduce_sum(masks_tiled * (mus + tf.exp(0.5 * 0) * epsilon), axis=1)
             
             log_likelihood_loss =  -tf.reduce_sum(self.loss_fn(labels=original_image, logits=likelihood), axis=[1,2,3])
-            log_true_conditional_density_distribution_loss = log_normal_pdf(posterior, 0.0, 0.0)
+            log_true_prior_density_distribution_loss = log_normal_pdf(posterior, 0.0, 0.0)
             log_posterior_conditional_density_distribution_loss = log_normal_pdf(posterior,
                                                                              posterior_mean,
                                                                              posterior_log_var)
             # MINIMIZING THE NEGATIVE ELBO IS LIKE MAXIMIZING THE ELBO
+            # WE USE MONTE CARLO ESTIMATION OF ELBO
             loss = (1.0/ tf.cast(batch_size, tf.float32)) * (1.0 / tf.cast(self.iterations, dtype=tf.float32)) *  \
                    -tf.reduce_mean(log_likelihood_loss + \
-                                   log_true_conditional_density_distribution_loss + \
+                                   log_true_prior_density_distribution_loss - \
                                    log_posterior_conditional_density_distribution_loss)
             
         generator_grads = tape.gradient(loss, self.generator.trainable_variables)
         generator_grads_clipped = [tf.clip_by_norm(gg, 5.0) for gg in generator_grads]
-        self.optimizer.apply_gradients(zip(generator_grads_clipped, self.generator.trainable_variables))
+        self.g_optimizer.apply_gradients(zip(generator_grads_clipped, self.generator.trainable_variables))
 
         likelihood = self.layer_normalization_likelihood(likelihood)
         posterior_gradient = tape.gradient(loss, posterior)
         posterior_gradient_clipped_layer_normalized = self.layer_normalization_posterior_gradient(posterior_gradient)
-        # posterior_gradient_clipped = tf.clip_by_norm(posterior_gradient , 5.0) 
-        # posterior_gradient_clipped_layer_normalized = self.layer_normalization_posterior_gradient(posterior_gradient_clipped)
+        mus_gradient = tape.gradient(loss, mus)
+        mus_gradient_layer_normalized = self.layer_normalization_mus_gradient(mus_gradient)
+        masks_gradient = tape.gradient(loss, masks)
+        masks_gradient_layer_normalized = self.layer_normalization_masks_gradient(masks_gradient)
 
         for i in tf.range(2, self.iterations + 1, 1):
             with tf.GradientTape(persistent=True) as tape:
@@ -328,7 +315,7 @@ class VAE(tfk.Model):
 
                 
                 log_likelihood_loss =  -tf.reduce_sum(self.loss_fn(labels=original_image, logits=likelihood), axis=[1,2,3])
-                log_true_conditional_density_distribution_loss = log_normal_pdf(posterior, 0.0, 0.0)
+                log_true_prior_density_distribution_loss = log_normal_pdf(posterior, 0.0, 0.0)
                 log_posterior_conditional_density_distribution_loss = log_normal_pdf(posterior,
                                                                                 posterior_mean,
                                                                                 posterior_log_var)
@@ -336,22 +323,23 @@ class VAE(tfk.Model):
                 # WE USE MONTE CARLO ESTIMATION OF ELBO
                 loss += (1.0 / tf.cast(batch_size, tf.float32)) * (tf.cast(i, dtype=tf.float32)/tf.cast(self.iterations, dtype=tf.float32)) *\
                        -tf.reduce_mean(log_likelihood_loss + \
-                                       log_true_conditional_density_distribution_loss + \
+                                       log_true_prior_density_distribution_loss - \
                                        log_posterior_conditional_density_distribution_loss)
                 
             generator_grads = tape.gradient(loss, self.generator.trainable_variables)
             generator_grads_clipped = [tf.clip_by_norm(gg, 5.0) for gg in generator_grads]
-            self.optimizer.apply_gradients(zip(generator_grads_clipped, self.generator.trainable_variables))
+            self.g_optimizer.apply_gradients(zip(generator_grads_clipped, self.generator.trainable_variables))
             refinement_grads = tape.gradient(loss, self.refinement.trainable_variables)
             refinement_grads_clipped = [tf.clip_by_norm(rg , 5.0) for rg in refinement_grads]
-            self.optimizer.apply_gradients(zip(refinement_grads_clipped, self.refinement.trainable_variables))
+            self.ref_optimizer.apply_gradients(zip(refinement_grads_clipped, self.refinement.trainable_variables))
             
             likelihood = self.layer_normalization_likelihood(likelihood)
             posterior_gradient = tape.gradient(loss, posterior)
             posterior_gradient_clipped_layer_normalized = self.layer_normalization_posterior_gradient(posterior_gradient)
-            # posterior_gradient_clipped = tf.clip_by_norm(posterior_gradient , 10.0)
-            # posterior_gradient_clipped_layer_normalized = self.layer_normalization_posterior_gradient(posterior_gradient_clipped)
-
+            mus_gradient = tape.gradient(loss, mus)
+            mus_gradient_layer_normalized = self.layer_normalization_mus_gradient(mus_gradient)
+            masks_gradient = tape.gradient(loss, masks)
+            masks_gradient_layer_normalized = self.layer_normalization_masks_gradient(masks_gradient)
         
         # COMPUTE OUR OWN METRICS
         loss_tracker.update_state(loss)
@@ -363,10 +351,6 @@ class VAE(tfk.Model):
         return [loss_tracker,]
 
 # %%
-# initial_learning_rate = 0.0003
-# learning_rate_schedule = tfk.optimizers.schedules.ExponentialDecay(
-#     initial_learning_rate, decay_steps=100, decay_rate=.9, staircase=True
-#     )
 
 checkpoint_cb = tfk.callbacks.ModelCheckpoint(
     'IODINE_model_on_tetrominoes.h5', monitor='loss',
@@ -374,18 +358,15 @@ checkpoint_cb = tfk.callbacks.ModelCheckpoint(
 )
 
 early_stopping_cb = tfk.callbacks.EarlyStopping(
-    monitor='loss', min_delta=0.001, patience=5, 
+    monitor='loss', min_delta=0.001, patience=50, 
     mode='auto', restore_best_weights=True
 )
 
-# @tf.function
-# def my_loss(y_true, y_pred):
-#     return tf.reduce_sum(tf.math.square(y_true - y_pred), axis=[1,2,3])
 
 vae = VAE(generator, refinement, image_size=IMAGE_SIZE, latent_dim=latent_dim, iterations=T, num_slots=K)
-vae.compile(optimizer=tfk.optimizers.Adam(learning_rate=0.0003),
+vae.compile(g_optimizer=tfk.optimizers.Adam(learning_rate=0.0003),
+            ref_optimizer=tfk.optimizers.Adam(learning_rate=0.0003),
             loss_fn=tf.nn.sigmoid_cross_entropy_with_logits)
-            # loss_fn=my_loss)
 # %%
 # LOADING THE DATASET
 # THIS IS A DATASET OF TETRIS-LIKE SHAPE (AKA TETROMINOES)
@@ -444,15 +425,12 @@ path = './data/tetrominoes_tetrominoes_train.tfrecords'
 
 dataset = dataset(path)
 batched_dataset = dataset.shuffle(1000).batch(batch_size)
-# validation_dataset = dataset.shuffle(2000).batch(batch_size)
 
 for data in batched_dataset.take(1):
     print(data.keys())
 
 # %%
-batched_dataset_take = batched_dataset.take(10) 
-# batched_valid_take = validation_dataset.take(2)
-vae.fit(batched_dataset_take,
+vae.fit(batched_dataset,
         callbacks=[early_stopping_cb,
                    checkpoint_cb],
         epochs=10, 
@@ -460,28 +438,28 @@ vae.fit(batched_dataset_take,
 
 vae.save_weights('tetrominoes.h5')
 # %%
-from typing import Any
-from dataclasses import dataclass
-@dataclass
-class EvaluationResults:
-    __slots__ = ['masks_logits',
-                 'mask',
-                 'mus',
-                 'likelihood']
+# from typing import Any
+# from dataclasses import dataclass
+# @dataclass
+# class EvaluationResults:
+#     __slots__ = ['masks_logits',
+#                  'mask',
+#                  'mus',
+#                  'likelihood']
 
-    masks_logits: Any
-    mask: Any
-    mus: Any
-    likelihood: Any
+#     masks_logits: Any
+#     mask: Any
+#     mus: Any
+#     likelihood: Any
 # %%
-er = EvaluationResults()
-vae.evaluate(batched_dataset_take_10, er)
+# er = EvaluationResults()
+# vae.evaluate(batched_dataset_take_10, er)
 # %%
-from sklearn.metrics.cluster import adjusted_rand_score
+# from sklearn.metrics.cluster import adjusted_rand_score
 # %%
 
-checkpoint = tf.train.Checkpoint(vae)
-checkpoint.restore('IODINE_model_on_tetrominoes.h5')
+# checkpoint = tf.train.Checkpoint(vae)
+# checkpoint.restore('IODINE_model_on_tetrominoes.h5')
 # %%
-generator.load_weights('tetrominoes.h5', by_name=True)
+# generator.load_weights('tetrominoes.h5', by_name=True)
 # %%
